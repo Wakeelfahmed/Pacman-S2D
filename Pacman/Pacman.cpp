5de162c03764a21585fdf4518a2677cbff9a2a33
Wakeel Furqan Ahmed - COE <wakeelfahmed@gmail.com>
2023-08-09 09:17:58 +0500

    fully optimized and ghost functionality enhanced

--- Pacman/Pacman.cpp
+++ Pacman/Pacman.cpp
@@ -6,20 +6,16 @@
 #include <iomanip>
 int PLAYABLE_HEIGHT;
 int PLAYABLE_WIDTH;
-Pacman::Pacman(int argc, char* argv[]) : Game(argc, argv), _cPacmanSpeed(0.1f), _cPacmanFrameTime(250)
+int Generate_rand_num_within_PlayableArea_Height() {
+	return  ((rand() % ((((Graphics::GetViewportHeight() - 30)) - 25) + 1)) + 25);	//leave 25 from top and 5 from bottom
+}
+Pacman::Pacman(int argc, char* argv[]) : Game(argc, argv)
 {
-	Pacman_Player = new Player();		_ghosts[0] = new MovingEnemy();		Cherry = new Collectable();
-	_frameCount = 0;
+	Pacman_Player = new Player();		Cherry = new Collectable();
 
-	_paused = false;
+	_frameCount = 0;	//Paused = false;
 	srand(time(NULL));
-	for (int i = 0; i < MUNCHIECOUNT; i++)
-	{
-		_munchies[i] = new Collectable();
-		_munchies[i]->frameCount = rand() % 1;
-		_munchies[i]->currentFrameTime = 0;
-		_munchies[i]->frameTime = rand() % 500 + 50;
-	}
+
 	Graphics::Initialise(argc, argv, this, 1024, 768, false, 25, 25, "Pacman", 60); 	//Initialise important Game aspects
 	Input::Initialise();
 	Graphics::StartGameLoop();	// Start the Game Loop - This calls Update and Draw in game loop 
@@ -28,46 +24,47 @@ Pacman::~Pacman()
 {
 	delete Pacman_Player->texture;	delete Pacman_Player->sourceRect;	delete Pacman_Player->position;
 	delete Cherry->texture;	delete Cherry->sourceRect;	delete Cherry->position;
-	delete _munchies[0]->texture;	delete _munchies[1]->texture;
+	delete Munchies[0]->texture;	delete Munchies[1]->texture;
 	for (int nCount = 0; nCount < MUNCHIECOUNT; nCount++)
 	{
-		delete _munchies[nCount]->position;
-		delete _munchies[nCount]->texture;
-		delete _munchies[nCount]->sourceRect;
-		delete _munchies[nCount];
+		delete Munchies[nCount]->position;
+		delete Munchies[nCount]->texture;
+		delete Munchies[nCount]->sourceRect;
+		delete Munchies[nCount];
 	}
-	delete[] _munchies;	//Order of operations is important, array deleted last
-}
-int generate_PlayableArea_Height() {
-	return  ((rand() % PLAYABLE_HEIGHT) + 25);	//leave 25 from top and 5 from bottom
+	delete[] Munchies;	//Order of operations is important, array deleted last
 }
 void Pacman::LoadContent()
 {
 	PLAYABLE_HEIGHT = (((Graphics::GetViewportHeight() - 30)) - 25) + 1;		PLAYABLE_WIDTH = (Graphics::GetViewportWidth() - 15) - 3 + 1;
-	Pacman_Player->Initialise_Player();		_ghosts[0]->Initialise_Ghost();//Player & Ghost all initial settings	
-	_score = 0;	_highScore = 0;	LoadHighScore();		// Initialize game state, score, and high score
+	Pacman_Player->Initialise_Player();		//Ghosts[0]->Initialise_Ghost();//Player & Ghost all initial settings	
+	Score = 0;	High_score = 0;	LoadHighScore();		// Initialize game state, score, and high score
 
 	_cherryInvertedTexture = new Texture2D();	_cherryInvertedTexture->Load("Textures/CherryInverted.png", false);		// Load Cherry Inverted
 	Cherry->texture = new Texture2D();	Cherry->texture->Load("Textures/Cherry.png", false);
 	Cherry->position = new Vector2((rand() % PLAYABLE_WIDTH) + 3, (rand() % PLAYABLE_HEIGHT) + 25);
-
+	for (int i = 0; i < GHOSTCOUNT; i++)
+	{
+		Ghosts[i] = new MovingEnemy();	//create obj of Ghost
+		Ghosts[i]->Initialise_Ghost(); //Ghost all initial settings	
+	}
 	_startMessageDisplayTime = 3000; /*3 seconds*/		_showStartMessage = true;
 
 	_startSound = new S2D::SoundEffect();	_startSound->Load("sound/pop.wav");
 
-	_munchies[0]->texture = new Texture2D();	_munchies[0]->texture->Load("Textures/Munchie.tga", true);// Load Munchies		keeping Muchie texture on 0 and MunchieInverted on 1, using only these in Draw()
-	_munchies[1]->texture = new Texture2D();	_munchies[1]->texture->Load("Textures/MunchieInverted.tga", true);// Load Munchies
 	for (int i = 0; i < MUNCHIECOUNT; i++) {
-		_munchies[i]->sourceRect = new Rect(((rand() % PLAYABLE_WIDTH) + 3), generate_PlayableArea_Height(), 12, 12);
-		_munchies[i]->position = new Vector2(_munchies[i]->sourceRect->X, _munchies[i]->sourceRect->Y);
+		Munchies[i] = new Collectable();
+		Munchies[i]->frameCount = rand() % 1;
+		Munchies[i]->currentFrameTime = 0;
+		Munchies[i]->frameTime = rand() % 500 + 50;
+		Munchies[i]->sourceRect = new Rect(((rand() % PLAYABLE_WIDTH) + 3), Generate_rand_num_within_PlayableArea_Height(), 12, 12);
+		Munchies[i]->position = new Vector2(Munchies[i]->sourceRect->X, Munchies[i]->sourceRect->Y);
 	}
-
-	// Set string position
-	_stringPosition = new Vector2(10.0f, 25.0f);
+	Munchies[1]->texture = new Texture2D();	Munchies[1]->texture->Load("Textures/MunchieInverted.tga", true);// Load Munchies
+	Munchies[0]->texture = new Texture2D();	Munchies[0]->texture->Load("Textures/Munchie.tga", true);// Load Munchies		keeping Muchie texture on 0 and MunchieInverted on 1, using only these in Draw()
 
 	// Set Menu Paramters
-	_menuBackground = new Texture2D();
-	_menuBackground->Load("Textures/Transparency.png", false);
+	_menuBackground = new Texture2D();	_menuBackground->Load("Textures/Transparency.png", false);
 	_menuRectangle = new Rect(0.0f, 0.0f, Graphics::GetViewportWidth(), Graphics::GetViewportHeight());
 	_menuStringPosition = new Vector2(Graphics::GetViewportWidth() / 2.0f, Graphics::GetViewportHeight() / 2.0f);
 }
@@ -84,30 +81,39 @@ void Pacman::Update(int elapsedTime)
 
 	if (!Pacman_Player->dead) // Only update the game if it's not over
 	{
-		UpdateCherryCollision();
+		for (int i = 0; i < 100000; i++) {}
 		if (keyboardState->IsKeyDown(Input::Keys::P) && !_pKeyDown)
 		{
 			_pKeyDown = true;
-			_paused = !_paused;
+			Paused = !Paused;
 		}
 		if (keyboardState->IsKeyUp(Input::Keys::P))
 			_pKeyDown = false;
 
-		if (!_paused)
+		UpdateCherryCollision();
+		if (!Paused || !Game_Compeleted)
 		{
-			if (Pacman_Player->speedMultiplier == 1)
+			int Munchies_Eaten = 0;
+			for (int i = 0; i < MUNCHIECOUNT; i++)
+				if (Munchies[i]->position->X == -1 && Munchies[i]->position->Y == -1)
+					Munchies_Eaten++;
+			if (Munchies_Eaten == MUNCHIECOUNT)
+				Game_Compeleted = 1;
+
+
+			if (Power_Time >= 0)
 			{
-				_cPacmanSpeed = 0.3f;
 				Power_Time++;
 				if (Power_Time >= 200) {
-					Pacman_Player->speedMultiplier = 0;
-					Power_Time = 0;
+					Pacman_Player->speedMultiplier = 0.1f;	//reset speed back to normal
+					Power_Time = -1;
 				}
 			}
 			else
-				_cPacmanSpeed = 0.1f;
+				Pacman_Player->speedMultiplier = 0.1f;
 			_frameCount++;
-			UpdateGhost(_ghosts[0], elapsedTime);
+			for (int i = 0; i < GHOSTCOUNT; i++)
+				UpdateGhost(Ghosts[i], elapsedTime);
 			CheckGhostCollisions();
 			// Checks if key is pressed
 			if (keyboardState->IsKeyDown(Input::Keys::RIGHT))
@@ -120,13 +126,13 @@ void Pacman::Update(int elapsedTime)
 				pacmanDirection = direction::Down;
 
 			if (pacmanDirection == direction::Right)
-				Pacman_Player->position->X += _cPacmanSpeed * elapsedTime;
+				Pacman_Player->position->X += Pacman_Player->speedMultiplier * elapsedTime;
 			else if (pacmanDirection == direction::Up)
-				Pacman_Player->position->Y -= _cPacmanSpeed * elapsedTime;
+				Pacman_Player->position->Y -= Pacman_Player->speedMultiplier * elapsedTime;
 			else if (pacmanDirection == direction::Left)
-				Pacman_Player->position->X -= _cPacmanSpeed * elapsedTime;
+				Pacman_Player->position->X -= Pacman_Player->speedMultiplier * elapsedTime;
 			else if (pacmanDirection == direction::Down)
-				Pacman_Player->position->Y += _cPacmanSpeed * elapsedTime;
+				Pacman_Player->position->Y += Pacman_Player->speedMultiplier * elapsedTime;
 
 			Pacman_Player->sourceRect->Y = Pacman_Player->sourceRect->Height * pacmanDirection;	//Changing Pacman Texture Direction
 			// Checks if Pacman is trying to disappear
@@ -139,7 +145,7 @@ void Pacman::Update(int elapsedTime)
 			if (Pacman_Player->position->Y < 0 - 0.5 * Pacman_Player->sourceRect->Height)
 				Pacman_Player->position->Y = Graphics::GetViewportHeight() - 0.5 * Pacman_Player->sourceRect->Height;
 
-			if (Pacman_Player->currentFrameTime > _cPacmanFrameTime)
+			if (Pacman_Player->currentFrameTime > 250)
 			{
 				Pacman_Player->frame++;
 				if (Pacman_Player->frame >= 2)
@@ -150,13 +156,9 @@ void Pacman::Update(int elapsedTime)
 				Pacman_Player->sourceRect->X = Pacman_Player->sourceRect->Width * Pacman_Player->frame;
 			}
 
-			// Check for collision with screen walls
-			if (Pacman_Player->position->X < 0 || Pacman_Player->position->X > Graphics::GetViewportWidth() || Pacman_Player->position->Y < 0 || Pacman_Player->position->Y > Graphics::GetViewportHeight())
-				Pacman_Player->dead = true;		//Player Collided with wall
-
 			for (int i = 0; i < MUNCHIECOUNT; i++)
-				if (Munchies_Collision(_munchies[i]))
-					UpdateMunchies(_munchies[i]);	//Munchie is eaten
+				if (Munchies_Collision(Munchies[i]))
+					UpdateMunchies(Munchies[i]);	//Munchie is eaten
 		}
 	}
 	else // Game is OVER!
@@ -166,22 +168,26 @@ void Pacman::Update(int elapsedTime)
 		{
 			// Restart the game
 			Pacman_Player->Initialise_Player();	// Reset Pacman Player
-			Cherry->position->X = (rand() % Graphics::GetViewportWidth()); Cherry->position->Y = generate_PlayableArea_Height(); // Reset cherry's position
+			Cherry->position->X = (rand() % Graphics::GetViewportWidth()); Cherry->position->Y = Generate_rand_num_within_PlayableArea_Height(); // Reset cherry's position
 			_frameCount = 0;
-			_paused = false;
+			Paused = false;		Game_Compeleted = false;
 			_showStartMessage = true;			_startMessageDisplayTime = 3000;
-			_score = 0;			LoadHighScore(); // Load high score from file
+			Score = 0;			LoadHighScore(); // Load high score from file
+			for (int i = 0; i < MUNCHIECOUNT; i++) {
+				Munchies[i]->sourceRect = new Rect(((rand() % PLAYABLE_WIDTH) + 3), Generate_rand_num_within_PlayableArea_Height(), 12, 12);
+				Munchies[i]->position = new Vector2(Munchies[i]->sourceRect->X, Munchies[i]->sourceRect->Y);
+			}
 		}
 	}
 }
 void Pacman::Draw(int elapsedTime)
 {
 	SpriteBatch::BeginDraw(); // Starts Drawing
-	if (Pacman_Player->dead) { DrawEndGame(); }
+
+	if (Pacman_Player->dead || Game_Compeleted) { DrawEndGame(); }
 	else
 	{
-		// Show "Start Game" message
-		if (_showStartMessage)
+		if (_showStartMessage)		// Show "Start Game" message
 		{
 			std::stringstream startMessageStream;
 			startMessageStream << "Start Game!";
@@ -190,9 +196,8 @@ void Pacman::Draw(int elapsedTime)
 			Vector2 startMessagePosition((Graphics::GetViewportWidth() - 5) / 2.0f, Graphics::GetViewportHeight() / 2.0f);
 			SpriteBatch::DrawString(startMessageStream.str().c_str(), &startMessagePosition, Color::Green);
 
-			// Check if it's time to hide the message
 			_startMessageDisplayTime -= elapsedTime;
-			if (_startMessageDisplayTime <= 0)
+			if (_startMessageDisplayTime <= 0)			// Check if it's time to hide the message
 				_showStartMessage = false;
 
 		}
@@ -202,12 +207,12 @@ void Pacman::Draw(int elapsedTime)
 			SpriteBatch::Draw(Pacman_Player->texture, Pacman_Player->position, Pacman_Player->sourceRect);
 			for (int i = 0; i < MUNCHIECOUNT; i++)			// Show munchie
 			{
-				if (_munchies[i]->position->X == -1)//Already eaten munchie
+				if (Munchies[i]->position->X == -1)//Already eaten munchie
 					continue;
 				if (_frameCount < 30)
-					SpriteBatch::Draw(_munchies[1]->texture, _munchies[i]->sourceRect, nullptr, Vector2::Zero, 1.0f, 0.0f, Color::White, SpriteEffect::NONE);	// Draw Red Munchie
+					SpriteBatch::Draw(Munchies[1]->texture, Munchies[i]->sourceRect, nullptr, Vector2::Zero, 1.0f, 0.0f, Color::White, SpriteEffect::NONE);	// Draw Red Munchie
 				else
-					SpriteBatch::Draw(_munchies[0]->texture, _munchies[i]->sourceRect, nullptr, Vector2::Zero, 1.0f, 0.0f, Color::White, SpriteEffect::NONE);		// Draw Blue Munchie
+					SpriteBatch::Draw(Munchies[0]->texture, Munchies[i]->sourceRect, nullptr, Vector2::Zero, 1.0f, 0.0f, Color::White, SpriteEffect::NONE);		// Draw Blue Munchie
 			}
 			if (_frameCount < 30)
 				SpriteBatch::Draw(_cherryInvertedTexture, Cherry->position);
@@ -219,13 +224,18 @@ void Pacman::Draw(int elapsedTime)
 				if (_frameCount >= 60)
 					_frameCount = 0;
 			}
-			SpriteBatch::Draw(_ghosts[0]->texture, _ghosts[0]->position, _ghosts[0]->sourceRect);
+			for (int i = 0; i < GHOSTCOUNT; i++)
+			{
+				SpriteBatch::Draw(Ghosts[0]->texture, Ghosts[i]->position, Ghosts[i]->sourceRect);
+
+			}
+			SpriteBatch::Draw(Ghosts[0]->texture, Ghosts[0]->position, Ghosts[0]->sourceRect);
 			// Draws String
 			std::stringstream stream;	stream << "Pacman X: " << std::fixed << std::setprecision(1) << Pacman_Player->position->X << "\tY: " << std::fixed << std::setprecision(1) << Pacman_Player->position->Y << std::endl; //retain only 1st decimal
-			SpriteBatch::DrawString(stream.str().c_str(), _stringPosition, Color::Green);
+			SpriteBatch::DrawString(stream.str().c_str(), &Vector2{ 10.0f, 20.0f }, Color::Green);
 
 			// Show "PAUSED!" message if the game is paused
-			if (_paused)
+			if (Paused)
 			{
 				std::stringstream menuStream;
 				menuStream << "PAUSED!";
@@ -236,7 +246,7 @@ void Pacman::Draw(int elapsedTime)
 
 		// Draw the score
 		std::stringstream scoreStream;
-		scoreStream << "Score: " << _score;
+		scoreStream << "Score: " << Score;
 		Vector2 scorePosition((Graphics::GetViewportWidth() - 3) / 2, 30.0f); // You can adjust the position of the score on the screen
 		SpriteBatch::DrawString(scoreStream.str().c_str(), &scorePosition, Color::White);
 	}
@@ -255,10 +265,10 @@ void Pacman::CheckGhostCollisions() {
 	for (int i = 0; i < GHOSTCOUNT; i++)
 	{
 		// Populate variables with Ghost data
-		bottom2 = _ghosts[i]->position->Y + _ghosts[i]->sourceRect->Height;
-		left2 = _ghosts[i]->position->X;
-		right2 = _ghosts[i]->position->X + _ghosts[i]->sourceRect->Width;
-		top2 = _ghosts[i]->position->Y;
+		bottom2 = Ghosts[i]->position->Y + Ghosts[i]->sourceRect->Height;
+		left2 = Ghosts[i]->position->X;
+		right2 = Ghosts[i]->position->X + Ghosts[i]->sourceRect->Width;
+		top2 = Ghosts[i]->position->Y;
 		if ((bottom1 > top2) && (top1 < bottom2) && (right1 > left2) && (left1 < right2))
 		{
 			Pacman_Player->dead = true;
@@ -267,14 +277,15 @@ void Pacman::CheckGhostCollisions() {
 	}
 }
 void Pacman::UpdateMunchies(Collectable* Munchie) {
-	Munchie->position->X = -1;		Munchie->position->Y = -1;
+	Munchie->position->X = -1;		Munchie->position->Y = -1;		//inactive eaten munchie
 	Munchie->sourceRect->X = -1;	Munchie->sourceRect->Y = -1;
+	Score += 5;
 }
 bool Pacman::Munchies_Collision(Collectable* Munchie) {
 	if (Pacman_Player->position->X + Pacman_Player->sourceRect->Width > Munchie->position->X &&
-		Pacman_Player->position->X < Munchie->position->X + _munchies[0]->texture->GetWidth() &&
+		Pacman_Player->position->X < Munchie->position->X + Munchies[0]->texture->GetWidth() &&
 		Pacman_Player->position->Y + Pacman_Player->sourceRect->Height > Munchie->position->Y &&
-		Pacman_Player->position->Y < Munchie->position->Y + _munchies[0]->texture->GetHeight())
+		Pacman_Player->position->Y < Munchie->position->Y + Munchies[0]->texture->GetHeight())
 
 		return true;
 
@@ -284,54 +295,57 @@ void Pacman::UpdateGhost(MovingEnemy* ghost, int elapsedTime)
 {
 	if (ghost->direction == 0) //Moves Right
 		ghost->position->X += ghost->speed * elapsedTime;
-	else if (ghost->direction == 1) //Moves Left
+	else if (ghost->direction == 1) //Moves down
+		ghost->position->Y += ghost->speed * elapsedTime;
+	else if (ghost->direction == 2) //Moves left
 		ghost->position->X -= ghost->speed * elapsedTime;
+	else if (ghost->direction == 3) //Moves up
+		ghost->position->Y -= ghost->speed * elapsedTime;
+
 	if (ghost->position->X + ghost->sourceRect->Width >= Graphics::GetViewportWidth()) //Hits Right edge
-		ghost->direction = 1; //Change direction
+		ghost->direction = 2; //Change direction
 	else if (ghost->position->X <= 0) //Hits left edge
 		ghost->direction = 0; //Change direction
+	else if (ghost->position->Y <= 0) //Hits top edge
+		ghost->direction = 1; //Change direction
+	if (ghost->position->Y + ghost->sourceRect->Width >= Graphics::GetViewportHeight()) //Hits bottom edge
+		ghost->direction = 3; //Change direction
+
 }
 void Pacman::DrawEndGame()
 {
-	if (_score >= _highScore)	// Check and update high score if necessary
+	if (Score >= High_score)	// Check and update high score if necessary
 	{
-		_highScore = _score;
+		High_score = Score;
 		SaveHighScore(); // Save the new high score to the file
 	}
 	std::stringstream endGameStream; // Draw the "End Game" screen with score and high score table
-	endGameStream << "GAME OVER\n";
+	if (Pacman_Player->dead)
+		endGameStream << "GAME OVER\n";
+	else
+		endGameStream << "GAME COMPLETE\n";
 	endGameStream << "Game Stats are:\n";
-	endGameStream << "Your Score: " << _score << "\n";
-	endGameStream << "High Score: " << _highScore << "\n";
+	endGameStream << "Your Score: " << Score << "\n";
+	endGameStream << "High Score: " << High_score << "\n";
 	endGameStream << "Press R to restart";
 
 	// You can customize the position and color of the message as needed
 	Vector2 endGamePosition(Graphics::GetViewportWidth() / 2.0f, Graphics::GetViewportHeight() / 2.0f);
-	SpriteBatch::DrawString(endGameStream.str().c_str(), &endGamePosition, Color::Red);
-
-	// Check for restart input
-	Input::KeyboardState* keyboardState = Input::Keyboard::GetState();
-	if (keyboardState->IsKeyDown(Input::Keys::R))
-	{
-		// Restart the game
-		Pacman_Player->Initialise_Player();	// Reset Pacman's Player
-		Cherry->position->X = (rand() % Graphics::GetViewportWidth()); Cherry->position->Y = generate_PlayableArea_Height(); // Reset cherry's position
-		_frameCount = 0;
-		_paused = false; 
-		_showStartMessage = true;		_startMessageDisplayTime = 3000;
-		_score = 0;		LoadHighScore(); //Reset Level Score & Load high score from file
-	}
+	if (Pacman_Player->dead)
+		SpriteBatch::DrawString(endGameStream.str().c_str(), &endGamePosition, Color::Red);
+	else
+		SpriteBatch::DrawString(endGameStream.str().c_str(), &endGamePosition, Color::Green);
 }
 void Pacman::UpdateScore()// Increment the score
 {
-	_score += 10; // Adjust the score increment as needed
+	Score += 10; // Adjust the score increment as needed
 }
 void Pacman::LoadHighScore()
 {
 	std::ifstream file("highscore.txt");
 	if (file.is_open())
 	{
-		file >> _highScore;
+		file >> High_score;
 		file.close();
 	}
 }
@@ -341,11 +355,11 @@ void Pacman::SaveHighScore()
 	if (file.is_open())
 	{
 		//file << "highscore=" << _highScore;
-		file << _highScore;
+		file << High_score;
 		if (!file.fail())
 		{
 			file.close();
-			std::cout << "High score saved: " << _highScore << std::endl;
+			std::cout << "High score saved: " << High_score << std::endl;
 		}
 		else
 			std::cout << "Error: Failed to write the high score." << std::endl;
@@ -365,8 +379,8 @@ void Pacman::UpdateCherryCollision()
 		UpdateScore();
 		// Respawn the cherry at a random position within the visible area
 		Cherry->position->X = rand() % (Graphics::GetViewportWidth() - Cherry->texture->GetWidth());
-		Cherry->position->Y = generate_PlayableArea_Height();
-		Pacman_Player->speedMultiplier = 1;		Pacman_Player->frame = _frameCount;
-		Power_Time = 0;
+		Cherry->position->Y = Generate_rand_num_within_PlayableArea_Height();
+		Pacman_Player->speedMultiplier = 0.3;		Power_Time = 0;	//increase the speed & setting the powerup start time
+		Pacman_Player->frame = _frameCount;
 	}
 }
--- Pacman/Pacman.h
+++ Pacman/Pacman.h
@@ -1,5 +1,4 @@
 #pragma once
-
 // If Windows and not in Debug, this will run without a console window
 // You can use this to output information when debugging using cout or cerr
 #ifdef WIN32 
@@ -8,27 +7,25 @@
 #endif
 #endif
 #define MUNCHIECOUNT 50
-#define GHOSTCOUNT 1
+#define GHOSTCOUNT 2
 
 #include "S2D/S2D.h"	// Just need to include main header file
 
 // Reduces the amount of typing by including all classes in S2D namespace
 using namespace S2D;
-
 // Declares the Pacman class which inherits from the Game class.
 // This allows us to overload the Game class methods to help us
 // load content, draw and update our game.
 struct Player
 {
 	float speedMultiplier;
-	int currentFrameTime;
-	int frame;
+	int currentFrameTime;	int frame;
 	Rect* sourceRect;
 	Texture2D* texture;
 	Vector2* position;
 	bool dead;
-	Player() { texture = new Texture2D(); position = new Vector2(350.0f, 350.0f);	dead = false;	sourceRect = new Rect(0, 0, 32, 32);	frame = 0;	currentFrameTime = 0; }
-	void Initialise_Player() { texture->Load("Textures/Pacman.tga", false);	dead = false;	position->X = position->Y = 350.0f;	sourceRect->X = sourceRect->Y = 0;	sourceRect->Height = sourceRect->Width = 32;	frame = 0;	currentFrameTime = 0; speedMultiplier = 0; }
+	Player() { texture = new Texture2D(); position = new Vector2(350.0f, 350.0f);	dead = false;	sourceRect = new Rect(0, 0, 32, 32);	frame = 0;	currentFrameTime = 0;	speedMultiplier = 0.1f; }
+	void Initialise_Player() { texture->Load("Textures/Pacman.tga", false);	dead = false;	position->X = position->Y = 350.0f;	sourceRect->X = sourceRect->Y = 0;	sourceRect->Height = sourceRect->Width = 32;	frame = 0;	currentFrameTime = 0; speedMultiplier = 0.1f; }
 };
 struct Collectable
 {
@@ -38,6 +35,7 @@ struct Collectable
 	Texture2D* texture;
 	Vector2* position;
 };
+int Generate_rand_num_within_PlayableArea_Height();
 struct MovingEnemy
 {
 	Vector2* position;
@@ -46,42 +44,37 @@ struct MovingEnemy
 	int direction;	float speed;
 	void Initialise_Ghost() {
 		texture = new Texture2D();	texture->Load("Textures/GhostBlue.png", false);	direction = 0;	speed = 0.2f;
-		position = new Vector2((rand() % Graphics::GetViewportWidth()), (rand() % Graphics::GetViewportHeight()));		sourceRect = new Rect(0, 0, 20, 20);
+		position = new Vector2((rand() % Graphics::GetViewportWidth()), Generate_rand_num_within_PlayableArea_Height());		sourceRect = new Rect(0, 0, 20, 20);
+		direction = rand() % 5;
 	}
 };
+
 enum direction { Right, Down, Left, Up };
 class Pacman : public Game
 {
 private:
-	int _highScore;			int _score;		// New variables for game state, score, and high score
-	//Update methods
+	int High_score;			int Score;		// New variables for game state, score, and high score
 	S2D::SoundEffect* _startSound;
 	Player* Pacman_Player;	int Power_Time;		direction pacmanDirection;// Data to represent Pacman
-	Collectable* _munchies[MUNCHIECOUNT];	// Data to represent Munchie
-	MovingEnemy* _ghosts[GHOSTCOUNT];
+	Collectable* Munchies[MUNCHIECOUNT];	// Data to represent Munchie
+	MovingEnemy* Ghosts[GHOSTCOUNT];
 	Collectable* Cherry;	Texture2D* _cherryInvertedTexture;
-
-	void CheckGhostCollisions();
-	void UpdateGhost(MovingEnemy*, int elapsedTime);
-	//Constant data for Game Variables 
-	float _cPacmanSpeed;	int _cPacmanFrameTime;
 
 	int _frameCount;
 
-	Vector2* _stringPosition;	// Position for String
 	bool _showStartMessage;		int _startMessageDisplayTime; // Declaration of the variable here
 
 	Texture2D* _menuBackground;	Rect* _menuRectangle;	Vector2* _menuStringPosition;		// Data for Menu
-	bool _paused;	bool _pKeyDown;
+	bool Paused{ false }, Game_Compeleted{ false };	bool _pKeyDown;
 
 	void Input(int elapsedTime, Input::KeyboardState* state);		//Input methods
 	void CheckPaused(Input::KeyboardState* state, Input::Keys pauseKey);		//Check methods
 	void CheckViewportCollision();		//Check methods
-	void UpdatePacman(int elapsedTime);
-	void UpdateMunchies(Collectable*);
-	bool Munchies_Collision(Collectable* Munchie);
+	void UpdatePacman(int elapsedTime);	//Update methods
+	void CheckGhostCollisions();			void UpdateGhost(MovingEnemy*, int elapsedTime);
+	void UpdateMunchies(Collectable*);		bool Munchies_Collision(Collectable* Munchie);
 	void UpdateCherryCollision();	// Function to handle cherry collision
-	void DrawEndGame();		// function to show the "End Game" screen
+	void DrawEndGame();		// function to show the "End Game" screen (Player dead / Game complete Screen)
 	void UpdateScore();	// Function to update the score and high score
 	void LoadHighScore();	// Function to load high score from a file
 	void SaveHighScore();	// Function to save high score to a file
